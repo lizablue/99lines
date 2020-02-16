6196119e2884bd059081d779eb6939f56bdf5e94
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-02-16 17:54:36 -0600

    final commit

--- 99scripts.js
+++ 99scripts.js
@@ -1,10 +1,11 @@
 let friends = ['Ana', 'Laur', 'Faith', 'Kim', 'Steph'];
 
 for(var x = 0; x < friends.length; x++) {
-    // console.log(friends[x] + " strikes one out, clears it all out, ")
     for (var i = 99; i > 2; i--) {
         console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i-1) + " lines of code in the file")
     };
+    // last couple lines were a little different because of singular/plural rules
+    // is there a more efficient way to do this?
     if (i = 2) {
         console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i-1) + " line of code in the file")
     };
