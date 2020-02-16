c0ec41858bab29f2e73705fad5962c0b7c8713bd
ELIZABETH <EMCCREIGHT90@GMAIL.COM>
2020-02-16 17:49:12 -0600

    490 lines of code

--- 99scripts.js
+++ 99scripts.js
@@ -1,2 +1,14 @@
 let friends = ['Ana', 'Laur', 'Faith', 'Kim', 'Steph'];
 
+for(var x = 0; x < friends.length; x++) {
+    // console.log(friends[x] + " strikes one out, clears it all out, ")
+    for (var i = 99; i > 2; i--) {
+        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i-1) + " lines of code in the file")
+    };
+    if (i = 2) {
+        console.log(i + " lines of code in the file, " + i + " lines of code; " + friends[x] + " strikes one out, clears it all out, " + (i-1) + " line of code in the file")
+    };
+    if (i = 1) {
+        console.log(i + " line of code in the file, " + i + " line of code; " + friends[x] + " strikes one out, clears it all out, no more lines of code in the file")
+    };
+};
